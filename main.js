9b838fa40c852e050f7e6415a355a085c07dbe90
pinargultekin <pinargultekin@yahoo.com>
2020-10-05 00:44:11 -0400

    all functions are done

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
     $("button").click(function(){
-        
+if ($("#btn").hasClass("night")){
+    day();
+}else if($("#btn").hasClass("day")){
+    night();
+}
+    });
+    function night(){
+        $("#btn").removeClass("day");
+        $("#btn").addClass("night");
+        $("#text")[0].innerHTML = "DARK MODE";
+        $("body").css("background-color", "#032b43da");
+        $("#toggle-btn").css("right", "3px");
+        $("#btn").css("background-color", "#032b43");  
+    }
+
+    function day(){
+        $("#btn").removeClass("night");
+        $("#btn").addClass("day");
+        $("#text")[0].innerHTML = "LIGHT MODE";
         $("body").css("background-color", "#0985e36e");
         $("#toggle-btn").css("right", "45px");
-        $("#btn").css("background-color", "#0984e3")
-    });  
+        $("#btn").css("background-color", "#0984e3");
+    }
   
 })
