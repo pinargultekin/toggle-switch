fe20e22206da7340f6a10d1b54968750fc36af94
pinargultekin <pinargultekin@yahoo.com>
2020-10-04 22:12:05 -0400

    one way functionality added

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        
+        $("body").css("background-color", "#0985e36e");
+        $("#toggle-btn").css("right", "45px");
+        $("#btn").css("background-color", "#0984e3")
+    });  
+  
+})
